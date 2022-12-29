444bdf60c3f5ed66b71c4ff8aedfeccb11568dc2
krishnachaturvedi1901 <krishna.chaturvedi1901@gmail.com>
2022-12-29 20:28:15 +0530

    added cart backend functionalty code

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -5,5 +5,13 @@ router.get('/cart',async(req,res)=>{
     const {page=1,pageLimit=5,sortOrder='asc',sortBy='price'}=req.query
     
 })
+router.patch('/:userId/cart/:productId', async(req,res)=>{
+    const {productId,userId}=req.params
+    const productData=req.body
+})
+
+router.delete('/:userId/cart/:productId', async(req,res)=>{
+    const {productId,userId}=req.params
+})
 
 module.exports=router
