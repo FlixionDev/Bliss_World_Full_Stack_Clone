365e9d5554cda7a1f80cabd6f4be76bf4c86fdd2
Ritik73011 <ritikkumarsingh730@gmail.com>
2022-12-30 01:03:06 +0530

    user login 80% done

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const cors = require('cors');
 const ConnectDB = require('./connection/connect');
 const app = express();
 const registerRouter = require('./controllers/usersRegister.controller');
-
+const loginRouter = require('./controllers/userLogin.controller');
 app.use(express.json())
 app.use(cors());
 
 app.use('/register',registerRouter);
+app.use('/login',loginRouter);
+
 ConnectDB().then(()=>{
     app.listen(4000,()=>{
         console.log("Databse is connected successfully with on the port http://localhost:4000");
