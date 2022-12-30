955cb7529d589ba348492277fd1a83f64ad05ef3
Harsh1518 <harshanand064@gmail.com>
2022-12-30 20:42:23 +0530

    update

--- src/Components/CartPage/Checkout.js
+++ src/Components/CartPage/Checkout.js
@@ -342,7 +342,6 @@ const Checkout = () => {
               </div>
               <div>Apply</div>
             </div>
-            {/* <button id="coupensub">Enter Coupon code</button> */}
           </div>
 
           <div id="finaldivv">
